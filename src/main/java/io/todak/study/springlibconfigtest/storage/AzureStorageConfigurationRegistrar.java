ca57979403a602557150c34455962d2373b3b9e4
todak-sun <tjsdydwn@gmail.com>
2021-05-30 23:32:02 +0900

    no message

--- src/main/java/io/todak/study/springlibconfigtest/storage/AzureStorageConfigurationRegistrar.java
+++ src/main/java/io/todak/study/springlibconfigtest/storage/AzureStorageConfigurationRegistrar.java
@@ -1,11 +1,15 @@
 package io.todak.study.springlibconfigtest.storage;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportAware;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.Set;
+
+@Slf4j
 @Configuration
 @ConditionalOnProperty(prefix = "spring.myconfig.azure", name = "enable", havingValue = "true")
 @EnableConfigurationProperties(AzureStorageProperty.class)
@@ -14,7 +18,8 @@ public class AzureStorageConfigurationRegistrar implements ImportAware {
 
     @Override
     public void setImportMetadata(AnnotationMetadata annotationMetadata) {
-
+        Set<String> annotationTypes = annotationMetadata.getAnnotationTypes();
+        log.info("annotationTypes : {}", annotationTypes);
     }
 
 
